f5fb405fbb50a2fa9b6e34e703b936f427d91d93
Mari Cruz Lopez Fernandez <maricruzlop@gmail.com>
2024-07-04 11:18:38 +0100

    Find and display the shortest track in a Playlist

--- routes.js
+++ routes.js
@@ -14,11 +14,14 @@ router.get("/playlist/:id", playlistController.index);
 router.post("/playlist/:id/addtrack", playlistController.addTrack);
 router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack);
+router.get("/playlist/:playlistid/edittrack/:trackid", trackController.index);
+router.post("/playlist/:playlistid/updatetrack/:trackid", trackController.update);
+
 router.get("/", accountsController.index);
+
 router.get("/login", accountsController.login);
 router.get("/signup", accountsController.signup);
 router.get("/logout", accountsController.logout);
 router.post("/register", accountsController.register);
 router.post("/authenticate", accountsController.authenticate);
-router.get("/playlist/:playlistid/edittrack/:trackid", trackController.index);
-router.post("/playlist/:playlistid/updatetrack/:trackid", trackController.update);
+
